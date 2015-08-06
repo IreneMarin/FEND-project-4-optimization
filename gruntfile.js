8fb5a74f6bc4f80d54b9b6cf59cb8b22350865ad
IreneMarin <irene@marin.cat>
2015-08-06 19:09:14 +0200

    Grunt changes

--- gruntfile.js
+++ gruntfile.js
@@ -1,64 +1,56 @@
 module.exports = function(grunt) {
-
-    // Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-mkdir');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-html');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-responsive-images');
+    require('load-grunt-tasks')(grunt);
     
     // Where we configure each plug-in
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         
-        // clean and create the folders that we use for the "new" files for the tasks
+        // clean and create the folders that we use for the new files (images) for the tasks
         clean: {
             dev: {
-                src: ['img/build'],
+                src: ['views/images/opt', 'img/opt'],
             },
         },
 
         mkdir: {
             dev: {
                 options: {
-                    create: ['img/build']
+                    create: ['views/images/opt', 'img/opt']
                 },
             },
         },
         
-        // to lint javascript (validate and detect errors) 
+        // lint javascript (validate and detect errors) 
         jshint: {
-	       all: [
-		      'Gruntfile.js',
-		      'js/*.js'
-	       ]
+	       all: {
+               options: {
+                   force: true
+               },
+               src: ['Gruntfile.js', 'js/*.js', 'views/js/*.js']
+           }
         },
         
-        // to lint css (validate and detect errors)
+        // lint css (validate and detect errors)
         csslint: {            
             lax: {
                 options: {
                     import: false
                 },
-                src: ['css/*.css']
+                src: ['css/*.css', 'views/css/*css']
             }
         },
         
-        // to lint html (validate and detect errors)
+        // lint html (validate and detect errors)
         htmllint: {
             all: {
                 options: {
                     force: true
                 },
-                src: ["*.html"]
+                src: ['*.html', 'views/pizza.html']
             }
         },
         
-        // to minify javascript
+        // minify javascript
         uglify: {
             build: {
                 src: 'js/perfmatters.js',
@@ -66,7 +58,7 @@ module.exports = function(grunt) {
             }
         },
         
-        // to minify all css from the "css" folder, adding the .min extension        
+        // minify all css from the "css" folder, adding the .min extension        
         cssmin: {
             target: {
                 files: [{
@@ -75,11 +67,17 @@ module.exports = function(grunt) {
                     src: ['*.css', '!*.min.css'],
                     dest: 'css',
                     ext: '.min.css'
+                },{
+                    expand: true,
+                    cwd: 'views/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'views/css',
+                    ext: '.min.css'
                 }]
             }
         },
         
-        // to resize images
+        // resize images from pizza
         responsive_images: {
             dev: {
                 options: {
@@ -93,27 +91,29 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     src: ['*.{gif,jpg,png,JPG,PNG}'],
-                    cwd: 'img/',
-                    dest: 'img/'
+                    cwd: 'views/images/',
+                    dest: 'views/images/'
                 }]
             }
         },
         
-        // to optimize our images
+        // optimize images from pizza
         imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/build/'
-                }]
+            static: {
+                options: {
+                    optimizationLevel: 3      
+                },
+                files: {
+                    'views/images/opt/pizza-116.png': 'views/images/pizza-116.png',
+                    'views/images/opt/pizzeria-116.jpg': 'views/images/pizzeria-116.jpg',
+                    'img/opt/profilepic.jpg': 'img/profilepic.jpg'
+                }
             }
         }
         
     });
     
-    // Where we tell Grunt what to do when we type "grunt" into the terminal.
+    // Where we tell Grunt what to do when we type "grunt" into the terminal
     grunt.registerTask('default', [
         'clean',
         'mkdir',
